242eb76d2ec941deb6023699b1388be3de8abccc
Çağrı Döşeyen <doseyenc@gmail.com>
2022-07-26 12:20:34 +0300

    Initial commit

---
+++ app/src/main/java/com/doseyenc/foodbook/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.doseyenc.foodbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
