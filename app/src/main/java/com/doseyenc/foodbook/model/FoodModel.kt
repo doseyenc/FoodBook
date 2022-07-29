fa4244356413a4a1f83490b08448ce255c4ef7db
Çağrı Döşeyen <doseyenc@gmail.com>
2022-07-29 12:45:58 +0300

    internet permission added, model refactored

--- app/src/main/java/com/doseyenc/foodbook/model/FoodModel.kt
+++ app/src/main/java/com/doseyenc/foodbook/model/FoodModel.kt
@@ -1,14 +1,21 @@
 package com.doseyenc.foodbook.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class FoodModel(
+    @SerializedName("gorsel")
     val image: String,
+    @SerializedName("isim")
     val foodName: String?,
+    @SerializedName("kalori")
     val foodCalory: String?,
+    @SerializedName("karbonhidrat")
     val foodCarbonHydrat: String?,
+    @SerializedName("protein")
     val foodProtein: String?,
+    @SerializedName("yag")
     val foodFat: String?
 ):Parcelable
