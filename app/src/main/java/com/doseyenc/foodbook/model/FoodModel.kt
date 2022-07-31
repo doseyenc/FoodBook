70a19d0624d25fdf06cd8573c360a091db99a406
Çağrı Döşeyen <doseyenc@gmail.com>
2022-07-31 18:24:43 +0300

    the model class is customized for the room

--- app/src/main/java/com/doseyenc/foodbook/model/FoodModel.kt
+++ app/src/main/java/com/doseyenc/foodbook/model/FoodModel.kt
@@ -1,21 +1,39 @@
 package com.doseyenc.foodbook.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+@Entity(tableName = "Foods")
 data class FoodModel(
+
+    @ColumnInfo(name = "Image")
     @SerializedName("gorsel")
     val image: String,
+
+    @ColumnInfo(name = "Name")
     @SerializedName("isim")
     val foodName: String?,
+
+    @ColumnInfo(name = "Calory")
     @SerializedName("kalori")
     val foodCalory: String?,
+
+    @ColumnInfo(name = "Carbonhydrate")
     @SerializedName("karbonhidrat")
     val foodCarbonHydrat: String?,
+
+    @ColumnInfo(name = "Protein")
     @SerializedName("protein")
     val foodProtein: String?,
+
+    @ColumnInfo(name = "fat")
     @SerializedName("yag")
     val foodFat: String?
-):Parcelable
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
