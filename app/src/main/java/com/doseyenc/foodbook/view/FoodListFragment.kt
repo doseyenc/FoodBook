ee709fd1dc4ddf44daf81de5a4fcf728d7bfb2d0
Çağrı Döşeyen <doseyenc@gmail.com>
2022-08-01 18:35:59 +0300

    Update FoodListFragment.kt

--- app/src/main/java/com/doseyenc/foodbook/view/FoodListFragment.kt
+++ app/src/main/java/com/doseyenc/foodbook/view/FoodListFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.doseyenc.foodbook.R
 import com.doseyenc.foodbook.adapter.FoodListAdapter
+import com.doseyenc.foodbook.databinding.FragmentFoodListBinding
 import com.doseyenc.foodbook.model.FoodModel
 import com.doseyenc.foodbook.viewmodel.FoodListViewModel
-import kotlinx.android.synthetic.main.fragment_food_list.*
 
 
 class FoodListFragment : Fragment(R.layout.fragment_food_list), FoodListAdapter.FoodClick {
     private lateinit var viewModel: FoodListViewModel
+    private lateinit var binding: FragmentFoodListBinding
     private val rv_adapter = FoodListAdapter(arrayListOf(), this)
 
 
@@ -25,8 +26,8 @@ class FoodListFragment : Fragment(R.layout.fragment_food_list), FoodListAdapter.
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_food_list, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_food_list,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,47 +35,53 @@ class FoodListFragment : Fragment(R.layout.fragment_food_list), FoodListAdapter.
         viewModel = ViewModelProvider(this).get(FoodListViewModel::class.java)
         setUpRecyclerView()
         viewModel.refreshData()
-        swipeRefreshLayout.setOnRefreshListener {
-            progressBar.visibility = View.VISIBLE
-            textViewError.visibility = View.GONE
-            rv_foodList.visibility = View.GONE
-            viewModel.refreshFromServer()
-            swipeRefreshLayout.isRefreshing = false
+        binding.apply {
+            swipeRefreshLayout.setOnRefreshListener {
+                progressBar.visibility = View.VISIBLE
+                textViewError.visibility = View.GONE
+                rvFoodList.visibility = View.GONE
+                viewModel.refreshFromServer()
+                swipeRefreshLayout.isRefreshing = false
+            }
         }
+
 
         observeLiveData()
 
     }
 
     private fun setUpRecyclerView() {
-        rv_foodList.layoutManager = LinearLayoutManager(context)
-        rv_foodList.adapter = rv_adapter
+        binding.apply {
+            rvFoodList.layoutManager=LinearLayoutManager(context)
+            rvFoodList.adapter = rv_adapter
+        }
+
     }
 
     private fun observeLiveData() {
         viewModel.foods.observe(viewLifecycleOwner) {
             it?.let {
-                rv_foodList.visibility = View.VISIBLE
+                binding.rvFoodList.visibility = View.VISIBLE
                 rv_adapter.updateList(it)
             }
         }
         viewModel.foodErrorMessage.observe(viewLifecycleOwner) {
             it?.let {
                 if (it) {
-                    textViewError.visibility = View.VISIBLE
+                    binding.textViewError.visibility = View.VISIBLE
                 } else {
-                    textViewError.visibility = View.GONE
+                    binding.textViewError.visibility = View.GONE
                 }
             }
         }
         viewModel.foodLoading.observe(viewLifecycleOwner) {
             it?.let {
                 if (it) {
-                    rv_foodList.visibility = View.GONE
-                    textViewError.visibility = View.GONE
-                    progressBar.visibility = View.VISIBLE
+                    binding.rvFoodList.visibility = View.GONE
+                    binding.textViewError.visibility = View.GONE
+                    binding.progressBar.visibility = View.VISIBLE
                 } else {
-                    progressBar.visibility = View.GONE
+                    binding.progressBar.visibility = View.GONE
                 }
             }
         }
