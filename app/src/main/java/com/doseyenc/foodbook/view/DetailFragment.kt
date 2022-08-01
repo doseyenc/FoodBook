f268451812b90cfeeb17d8b2eb3fe1e6c57e0839
Çağrı Döşeyen <doseyenc@gmail.com>
2022-08-01 18:14:44 +0300

    completed

--- app/src/main/java/com/doseyenc/foodbook/view/DetailFragment.kt
+++ app/src/main/java/com/doseyenc/foodbook/view/DetailFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.doseyenc.foodbook.R
+import com.doseyenc.foodbook.databinding.FragmentDetailBinding
+import com.doseyenc.foodbook.databinding.RvItemDesignBinding
 import com.doseyenc.foodbook.util.createPlaceHolder
 import com.doseyenc.foodbook.util.setImage
 import com.doseyenc.foodbook.viewmodel.FoodDetailViewModel
@@ -20,17 +23,21 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     private lateinit var viewModel: FoodDetailViewModel
     private val args by navArgs<DetailFragmentArgs>()
     private  var clickedFoodId:Int =-1
+    private lateinit var binding: FragmentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail,container,false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         args.let {
             clickedFoodId = args.id
+
             Log.e("id",clickedFoodId.toString())
         }
         viewModel = ViewModelProvider(this).get(FoodDetailViewModel::class.java)
@@ -40,12 +47,7 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     private fun observeLiveData() {
         viewModel.food.observe(viewLifecycleOwner) { food ->
             food?.let {
-                textViewFoodName.text = food.foodName
-                textViewCalories.text = food.foodCalory
-                textVievCarbohydrate.text = food.foodCarbonHydrat
-                textVievFat.text = food.foodFat
-                textVievProtein.text = food.foodProtein
-                imageViewFood.setImage(food.image,createPlaceHolder(requireContext()))
+                binding.foodDetail = food
             }
         }
     }
